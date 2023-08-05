879d7c23eee527bf040aa33ff011d96a42d993d0
Billy1437 <132896159+Billy1437@users.noreply.github.com>
2023-08-05 19:58:03 +0630

    Add files via upload

--- gather_data.py
+++ gather_data.py
@@ -3,6 +3,7 @@ import requests
 from bs4 import BeautifulSoup
 import urllib.request
 import time
+from PIL import Image
 
 def create_brand_folder(brand):
     brand_folder = f"Images/{brand.lower().replace(' ', '_')}_images"
@@ -10,7 +11,7 @@ def create_brand_folder(brand):
         os.makedirs(brand_folder)
     return brand_folder
 
-def download_images_from_google(query, brand_folder, num_images=200):
+def download_images_from_google(query, brand_folder, num_images=100):
     search_url = f"https://www.google.com/search?q={query}&tbm=isch"
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"}
@@ -19,18 +20,7 @@ def download_images_from_google(query, brand_folder, num_images=200):
 
     image_urls = []
     for img in soup.find_all("img"):
-        if img.get("src"):
-            image_urls.append(img["src"])
-
-    num_attempts = num_images // len(image_urls) + 1
-    for _ in range(num_attempts):
-        response = requests.get(search_url + f"&start={len(image_urls)}", headers=headers)
-        soup = BeautifulSoup(response.content, "html.parser")
-        for img in soup.find_all("img"):
-            if img.get("src"):
-                image_urls.append(img["src"])
-
-        time.sleep(1)  # Add a delay between requests to avoid overloading the website
+        image_urls.append(img["src"])
 
     count = 0
     for i, img_url in enumerate(image_urls):
@@ -39,6 +29,19 @@ def download_images_from_google(query, brand_folder, num_images=200):
             urllib.request.urlretrieve(img_url, img_path)
             count += 1
             print(f"Downloaded {img_path}")
+
+            # Open the downloaded image
+            with Image.open(img_path) as img:
+                # Rotate the image by 45 degrees and save it
+                img_rotated_45 = img.rotate(45)
+                img_rotated_45_path = os.path.join(brand_folder, f"{i}_rotated_45.jpg")
+                img_rotated_45.save(img_rotated_45_path)
+
+                # Rotate the image by 120 degrees and save it
+                img_rotated_120 = img.rotate(120)
+                img_rotated_120_path = os.path.join(brand_folder, f"{i}_rotated_120.jpg")
+                img_rotated_120.save(img_rotated_120_path)
+
             if count >= num_images:
                 break
             time.sleep(1)  # Add a delay between image downloads to avoid overloading the website
@@ -46,9 +49,9 @@ def download_images_from_google(query, brand_folder, num_images=200):
             print(f"Error downloading image {i}: {e}")
 
 if __name__ == "__main__":
-    brands = ["Heineken", "Carlsberg", "Tiger", "Red Bull", "Coca Cola", "Pepsi"]  # Add more brands as needed
+    brands = ["Heineken", "Carlsberg", "Tiger"]  # Add more brands as needed
 
     for brand in brands:
         brand_folder = create_brand_folder(brand)
         query = f"{brand} can"  # You can modify the query as per your requirement
-        download_images_from_google(query, brand_folder, num_images=200)
+        download_images_from_google(query, brand_folder, num_images=100)
