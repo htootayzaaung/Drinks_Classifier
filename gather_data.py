bc6ffc0dcdd2e276da8227e6a2e34cc565a752ae
htootayzaaung <htootayzaaung.01@gmail.com>
2023-08-05 19:46:39 +0630

    Poor quality images but 200 images downloaded...

--- gather_data.py
+++ gather_data.py
@@ -10,7 +10,7 @@ def create_brand_folder(brand):
         os.makedirs(brand_folder)
     return brand_folder
 
-def download_images_from_google(query, brand_folder, num_images=100):
+def download_images_from_google(query, brand_folder, num_images=200):
     search_url = f"https://www.google.com/search?q={query}&tbm=isch"
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"}
@@ -19,7 +19,18 @@ def download_images_from_google(query, brand_folder, num_images=100):
 
     image_urls = []
     for img in soup.find_all("img"):
-        image_urls.append(img["src"])
+        if img.get("src"):
+            image_urls.append(img["src"])
+
+    num_attempts = num_images // len(image_urls) + 1
+    for _ in range(num_attempts):
+        response = requests.get(search_url + f"&start={len(image_urls)}", headers=headers)
+        soup = BeautifulSoup(response.content, "html.parser")
+        for img in soup.find_all("img"):
+            if img.get("src"):
+                image_urls.append(img["src"])
+
+        time.sleep(1)  # Add a delay between requests to avoid overloading the website
 
     count = 0
     for i, img_url in enumerate(image_urls):
@@ -35,9 +46,9 @@ def download_images_from_google(query, brand_folder, num_images=100):
             print(f"Error downloading image {i}: {e}")
 
 if __name__ == "__main__":
-    brands = ["Heineken", "Carlsberg", "Tiger"]  # Add more brands as needed
+    brands = ["Heineken", "Carlsberg", "Tiger", "Red Bull", "Coca Cola", "Pepsi"]  # Add more brands as needed
 
     for brand in brands:
         brand_folder = create_brand_folder(brand)
         query = f"{brand} can"  # You can modify the query as per your requirement
-        download_images_from_google(query, brand_folder, num_images=100)
+        download_images_from_google(query, brand_folder, num_images=200)
