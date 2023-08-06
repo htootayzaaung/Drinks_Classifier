8e0be17e59d8cbf47a9d970baffa3f0e19529a24
htootayzaaung <htootayzaaung.01@gmail.com>
2023-08-06 12:50:00 +0630

    Flickr API fully working

--- gather_data.py
+++ gather_data.py
@@ -4,12 +4,15 @@ from bs4 import BeautifulSoup
 import urllib.request
 import time
 from PIL import Image
+import flickrapi
 
 def create_brand_folder(brand):
     brand_folder = f"Images/{brand.lower().replace(' ', '_')}_images"
     if not os.path.exists(brand_folder):
         os.makedirs(brand_folder)
     return brand_folder
+
+#############################################       GOOGLE IMAGES       #############################################
 
 def download_images_from_google(query, brand_folder, num_images=100):
     search_url = f"https://www.google.com/search?q={query}&tbm=isch"
@@ -60,6 +63,13 @@ def download_images_from_google(query, brand_folder, num_images=100):
         except Exception as e:
             print(f"Error downloading image {i}: {e}")
 
+
+#############################################       END OF GOOGLE IMAGES       #############################################
+
+
+
+#############################################       UNSPLASH       #############################################
+
 def download_images_from_unsplash(query, brand_folder, num_images=100, access_key=None):
     api_url = f"https://api.unsplash.com/search/photos"
     headers = {
@@ -105,6 +115,46 @@ def download_images_from_unsplash(query, brand_folder, num_images=100, access_ke
     else:
         print(f"Failed to fetch images. HTTP {response.status_code}")
 
+
+#############################################       END OF UNSPLASH       #############################################
+
+
+#############################################        FLICKR       #############################################
+
+def download_images_from_flickr(query, brand_folder, num_images=100, api_key=None, api_secret=None):
+    flickr = flickrapi.FlickrAPI(api_key, api_secret, format='parsed-json')
+    photos = flickr.photos.search(text=query, per_page=num_images, sort='relevance')
+
+    for i, photo in enumerate(photos['photos']['photo']):
+        photo_url = f"https://farm{photo['farm']}.staticflickr.com/{photo['server']}/{photo['id']}_{photo['secret']}.jpg"
+        try:
+            img_path = os.path.join(brand_folder, f"flickr_{i}.jpg")
+            img_response = requests.get(photo_url)
+            if img_response.status_code == 200:
+                with open(img_path, "wb") as f:
+                    f.write(img_response.content)
+                print(f"Downloaded {img_path}")
+
+                # Open the downloaded image
+                with Image.open(img_path) as img:
+                    # Rotate the image by 45 degrees and save it
+                    img_rotated_45 = img.rotate(45)
+                    img_rotated_45_path = os.path.join(brand_folder, f"flickr_{i}_rotated_45.jpg")
+                    img_rotated_45.save(img_rotated_45_path)
+
+                    # Rotate the image by 120 degrees and save it
+                    img_rotated_120 = img.rotate(120)
+                    img_rotated_120_path = os.path.join(brand_folder, f"flickr_{i}_rotated_120.jpg")
+                    img_rotated_120.save(img_rotated_120_path)
+            else:
+                print(f"Error downloading image {i}: HTTP {img_response.status_code}")
+        except Exception as e:
+            print(f"Error downloading image {i}: {e}")
+        time.sleep(1)  # Add a delay between image downloads to avoid overloading the API
+
+
+#############################################       END OF FLICKR       #############################################
+
 if __name__ == "__main__":
     brands = [
         "Heineken beer",
@@ -116,9 +166,14 @@ if __name__ == "__main__":
     ]  # Add more brands as needed
 
     access_key = "M9JmKwZ5nDm20hVbXpbJH8ju1bMSLtmsp7CovYk6UBo"  # Replace with your actual Unsplash access key
+    api_key = "7dffe0a73bba4d9e5671fb934e2a6d62"  # Replace with your actual Flickr API key
+    api_secret = "b1e876ee5fb6acbd"  # Replace with your actual Flickr API secret
+
 
     for brand in brands:
         brand_folder = create_brand_folder(brand)
         query = brand  # Use the brand name itself for Red Bull, Coca Cola, and Pepsi
-        download_images_from_unsplash(query, brand_folder, num_images=100, access_key=access_key)
-        download_images_from_google(query, brand_folder, num_images=100)
+        #download_images_from_unsplash(query, brand_folder, num_images=100, access_key=access_key)
+        #download_images_from_google(query, brand_folder, num_images=100)
+        download_images_from_flickr(query, brand_folder, num_images=100, api_key=api_key, api_secret=api_secret)
+        
